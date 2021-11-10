ece42f59d6421bd13fa792096ea34a423e413d95
Charles Desiderio <cjdesi@gmail.com>
2021-11-10 07:41:30 -0600

    Testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
 
-function App() {
+  const callApi = () => {
+    fetch('https://vercel-api-test1.vercel.app/api/fetch.js')
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={callApi}>Fetch</button>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
